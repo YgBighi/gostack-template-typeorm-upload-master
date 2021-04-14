2f937ac7578bd4554a8e8195288fe03187216fcc
Yago Vieira <yagomanager@gmail.com>
2021-04-14 15:12:31 -0300

    first commit

---
+++ src/services/CreateTransactionService.ts
@@ -0,0 +1,63 @@
+import { getCustomRepository, getRepository } from 'typeorm';
+import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+class CreateTransactionService {
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const categoryRepository = getRepository(Category);
+
+    // VERIFICAR SE TEM SALDO SUFICIENTE NO BALANCE
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance.');
+    }
+
+    // VERIFICAR A CATEGORIA JA EXISTE
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    // Existe? Buscar ela do BD usar o ID que foi retornado
+    if (!transactionCategory) {
+      transactionCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactionCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: transactionCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
